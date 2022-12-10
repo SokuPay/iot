6dcb97b662708ae26e599e1e908f8fb57009b00b
shiroperu <pepper.dip.jp@gmail.com>
2022-12-10 20:24:26 +0900

    clenium 4対応

--- switch_trigger.py
+++ switch_trigger.py
@@ -3,6 +3,7 @@ import RPi.GPIO as GPIO
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # set GPIO connection
 GPIO.setmode(GPIO.BCM)
@@ -13,7 +14,8 @@ GPIO.setup(18,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 options = webdriver.ChromeOptions()
 options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=options)
+service = Service(executable_path='/usr/bin/chromedriver')
+browser = webdriver.Chrome(service=service,options=options)
 
 #elem = browser.find_element_by_name('inquire')
 
